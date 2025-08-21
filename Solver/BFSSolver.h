5061a5b2a78b200b6529295f57f37321a7ae19a7
RUETATA <138284511+RUETATA@users.noreply.github.com>
2025-08-21 13:47:13 +0530

    model and solver

---
+++ Solver/BFSSolver.h
@@ -0,0 +1,16 @@
+//
+// Created by ratan on 19-08-2025.
+//
+
+#ifndef BFSSOLVER_H
+#define BFSSOLVER_H
+
+
+
+class BFSSolver {
+
+};
+
+
+
+#endif //BFSSOLVER_H
---
+++ Solver/DFSSolver.h
@@ -0,0 +1,16 @@
+//
+// Created by ratan on 19-08-2025.
+//
+
+#ifndef DFSSOLVER_H
+#define DFSSOLVER_H
+
+
+
+class DFSSolver {
+
+};
+
+
+
+#endif //DFSSOLVER_H
